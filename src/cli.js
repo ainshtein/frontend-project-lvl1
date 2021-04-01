12eec2787a712d5acdf3b7ee3d3103c798382adb
ainshtein <andrej.amelichkin@gmail.com>
2021-04-01 15:44:59 +0300

    add greeting function

---
+++ src/cli.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+
+export const greeting = () => {
+    const name = readlineSync.question('May I have your name? ');
+
+    if (name) {
+        console.log(`Hello, ${name}`);
+    } else {
+        console.log('Hello, user');
+    }
+};
+
